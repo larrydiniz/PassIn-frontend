29337a1defb524eeffca0b8a363a618d95b130f9
lari <alarissadiniz@outlook.com>
2024-04-07 17:30:27 -0300

    feat: second class - components and pagination

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,12 @@
 import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+import { NavLink } from "./nav-link";
 export function Header() {
   return (
     <div className="flex items-center gap-5 py-2">
       <img src={nlwUniteIcon} alt="icon" />
       <nav className="flex items-center gap-5">
-        <a className="font-medium text-sm text-slate-400" href="">
-          Eventos
-        </a>
-        <a className="font-medium text-sm" href="">
-          Participantes
-        </a>
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </div>
   );
